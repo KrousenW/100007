33c44aa65cdd0e505bb6543f2c3959d2c38e738d
user <user@WIN-8NMEU04HFDT>
2022-02-08 16:08:48 +0300

    Добавьте файлы проекта.

---
+++ 1000-7/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Я гуль у меня 4....");
+int x = 1000;
+while (x > 0)
+{
+   Console.WriteLine(Convert.ToString(x),"-7=");
+   x = x - 7;
+   Thread.Sleep(50);
+}
+Console.ReadKey();
